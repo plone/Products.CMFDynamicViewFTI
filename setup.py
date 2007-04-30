8460c9010d3ef41974a7f43d1145dfe177fd53c3
Hanno Schlichting <hanno@hannosch.eu>
2007-04-30 21:42:57 +0000

    Moved documentation and added setup.py
    svn path=/CMFDynamicViewFTI/trunk/; revision=41361

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '3.0'
+
+setup(name='Products.CMFDynamicViewFTI',
+      version=version,
+      description="CMFDynamicViewFTI is a product for dynamic views in CMF.",
+      long_description="""\
+      """,
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+      ],
+      keywords='Zope CMF Plone dynamic view',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/collective/CMFDynamicViewFTI/trunk',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      download_url='http://plone.org/products/cmfdynamicviewfti/releases',
+      install_requires=[
+        'setuptools',
+      ],
+)
