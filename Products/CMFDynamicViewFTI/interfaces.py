e8ab7a0a97f790110606db84800e58db342c3982
Hanno Schlichting <hanno@hannosch.eu>
2008-07-08 15:33:09 +0000

    Adjusted deprecation warnings to point to Plone 4.0 instead of Plone 3.5 since we changed the version numbering again.
    svn path=/CMFDynamicViewFTI/branches/3.0/; revision=68061

--- Products/CMFDynamicViewFTI/interfaces.py
+++ Products/CMFDynamicViewFTI/interfaces.py
@@ -1,4 +1,4 @@
-# BBB module will be removed with Plone 3.5
+# BBB module will be removed with Plone 4.0
 #     zope 2 interfaces created at runtime:
 #     - IDynamicViewTypeInformation
 #     - IBrowserDefault
