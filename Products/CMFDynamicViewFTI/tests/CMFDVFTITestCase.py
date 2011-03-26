8e41945602adb5a9ab8a7ba62da30532afd06858
David Glick <davidglick@groundwire.org>
2011-03-26 02:00:53 +0000

    the sample types profile needs to get installed after the ZCA is cleared, now that GS registers profiles as components
    svn path=/CMFDynamicViewFTI/trunk/; revision=236509

--- Products/CMFDynamicViewFTI/tests/CMFDVFTITestCase.py
+++ Products/CMFDynamicViewFTI/tests/CMFDVFTITestCase.py
@@ -3,20 +3,24 @@
 #
 
 from Products.CMFTestCase.ctc import *
+from Products.CMFTestCase.layer import onsetup
 
 from Products.CMFCore.interfaces import ISiteRoot
 from Products.GenericSetup import EXTENSION, profile_registry
 
 installProduct('CMFDynamicViewFTI')
 
-profile_registry.registerProfile('CMFDVFTI_sampletypes',
-    'CMFDynamicViewFTI Sample Content Types',
-    'Extension profile including CMFDVFTI sample content types',
-    'profiles/sample_types',
-    'CMFDynamicViewFTI',
-    EXTENSION,
-    for_=ISiteRoot)
+@onsetup
+def setupSampleTypeProfile():
+    profile_registry.registerProfile('CMFDVFTI_sampletypes',
+        'CMFDynamicViewFTI Sample Content Types',
+        'Extension profile including CMFDVFTI sample content types',
+        'profiles/sample_types',
+        'CMFDynamicViewFTI',
+        EXTENSION,
+        for_=ISiteRoot)
 
+setupSampleTypeProfile()
 setupCMFSite(extension_profiles=['CMFDynamicViewFTI:CMFDVFTI_sampletypes'])
 
 class CMFDVFTITestCase(CMFTestCase):
