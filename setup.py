68a69e555f60fbdf7afa770ec9ab6e4c61821706
esteele <eric@esteele.net>
2018-06-20 08:58:15 -0400

    Back to development: 5.0.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '5.0.0'
+version = '5.0.1.dev0'
 
 long_description = open("README.rst").read()
 long_description += "\n"
