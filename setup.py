347a7e06fa41fc321e3e12a8ce7d23de5e65a577
Gil Forcada <gil.gnome@gmail.com>
2019-02-08 13:39:41 +0100

    Back to development: 6.0.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '6.0.1'
+version = '6.0.2.dev0'
 
 long_description = open("README.rst").read()
 long_description += "\n"
